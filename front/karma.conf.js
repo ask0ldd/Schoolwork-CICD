1f70fae120c73a2d7b58f1996471f0e11015996f
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-26 08:24:39 +0200

    *edit* front / github action : code coverage fix

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,10 +29,15 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
-      ]
+        { type: 'text-summary' },
+        { type: 'lcov'},
+      ],
+      /*coverageReporter: {
+        type: 'lcov',
+        dir: 'coverage/'
+      }*/
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
